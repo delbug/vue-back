e9cef14aaf5b9152b569467fe6fce1f38d75a031
delbug <81692059@qq.com>
2021-09-02 16:20:36 +0800

    change

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ let users = [
     { id: 2, name: 'jack', email: '222@qq.com' },
     { id: 3, name: 'mack', email: '333@qq.com' },
     { id: 4, name: 'summy', email: '444@qq.com' },
-    { id: 5, name: 'toni', email: '555@qq.com' },
+    { id: 5, name: 'toni', email: '5555@qq.com' },
 ];
-// 1
+// 1 2
 let server = http.createServer(function (req, res) {
     if (req.url === '/api/users') {
         res.setHeader("Access-Control-Allow-Origin", "*");
